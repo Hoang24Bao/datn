002370ef14e8e22df9e41f6e7e62e6c6e63a86eb
Nguyen Hoang <hoang2004bao@gmail.com>
2026-03-02 09:14:23 +0700

    Initial commit

---
+++ src/main/java/com/example/Controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/courses")
+    public String courses() {
+        return "courses";
+    }
+}
